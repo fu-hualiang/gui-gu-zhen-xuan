b8009f5bb6e8a52a2227f4b352ee9ce036a91df1
fuhualiang <hualiang.fu@foxmail.com>
2023-07-19 16:51:41 +0800

    ...

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 import SvgIcon from './SvgIcon/index.vue';
 
-const allGlobalComponent: Record<string,any> = { SvgIcon };
+const allGlobalComponent: Record<string, any> = { SvgIcon };
 export default {
   install(app: any) {
     Object.keys(allGlobalComponent).forEach((key) => {
